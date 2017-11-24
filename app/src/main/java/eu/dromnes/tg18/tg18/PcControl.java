eac9365c3c7bf1c34a894f853cfbbdcc90100419
Kodemikkel <martin.krisknut@hotmail.com>
2017-11-25 00:16:14 +0100

    Add pages with layout
    (No navigation)
    
    Signed-off-by: Kodemikkel <martin.krisknut@hotmail.com>

---
+++ app/src/main/java/eu/dromnes/tg18/tg18/PcControl.java
@@ -0,0 +1,13 @@
+package eu.dromnes.tg18.tg18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PcControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pc_control);
+    }
+}
