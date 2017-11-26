d79868e358992f34ac6b47c1d6a0832924cfb9d9
Kodemikkel <martin.krisknut@hotmail.com>
2017-11-26 16:48:17 +0100

    Add navigation
    Add navigation and convert activities to fragments
    to work as intended with navigation.
    
    Signed-off-by: Kodemikkel <martin.krisknut@hotmail.com>

--- app/src/main/java/eu/dromnes/tg18/tg18/MainActivity.java
+++ app/src/main/java/eu/dromnes/tg18/tg18/MainActivity.java
@@ -1,13 +1,56 @@
 package eu.dromnes.tg18.tg18;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+        implements LightControl.OnFragmentInteractionListener, HeightControl.OnFragmentInteractionListener, PcControl.OnFragmentInteractionListener {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_lightControl:
+                    transaction.replace(R.id.content, new LightControl()).commit();
+                    return true;
+                case R.id.navigation_heightControl:
+                    transaction.replace(R.id.content, new HeightControl()).commit();
+                    return true;
+                case R.id.navigation_pcControl:
+                    transaction.replace(R.id.content, new PcControl()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content, new LightControl()).commit();
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+        
     }
 }
